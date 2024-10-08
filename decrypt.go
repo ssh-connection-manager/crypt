9e044de3e26d343a05cde640f9fafc898a1c21ba
deniskorbakov <korbakovd@gmail.com>
2024-10-08 15:46:39 +0300

    fix: delete extra arguments

--- decrypt.go
+++ decrypt.go
@@ -2,8 +2,8 @@ package crypt
 
 import "errors"
 
-func Decrypt(str string, pathConf string, fileNameKey string) (string, error) {
-	cryptKey, err := GetKey(pathConf, fileNameKey)
+func Decrypt(str string) (string, error) {
+	cryptKey, err := GetKey()
 	if err != nil {
 		return "", errors.New("err at get crypt key")
 	}
